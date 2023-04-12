5296116140495da607842d3ddb063f84ac37bcb1
hywn <14hhan@gmail.com>
2023-04-12 22:15:41 +0900

    fix: 병명 한국어로 변경

--- api/main.py
+++ api/main.py
@@ -7,12 +7,12 @@ import tensorflow_addons as tfa
 
 MODEL = keras.models.load_model('models/ResNet152V2-600.h5', custom_objects={'Addons': tfa.metrics.F1Score(num_classes=6)})
 CLASS_DICT = {
-    0: 'scab',
-    1: 'healthy',
-    2: 'frog_eye_leaf_spot',
-    3: 'rust',
-    4: 'complex',
-    5: 'powdery_mildew',
+    0: '더뎅이병, 검은별무늬병', # scab
+    1: '건강함', # healthy
+    2: '점무늬병', # frog_eye_leaf_spot
+    3: '녹병', # rust
+    4: '복합성 질병', # complex
+    5: '흰가루병', # powdery_mildew
 }
 
 app = FastAPI()
