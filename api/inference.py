22cb2eaef575e64a1fe41fadc6d6b0a533b445bd
hywn <14hhan@gmail.com>
2023-05-23 15:36:00 +0900

    fix: verify image

--- api/inference.py
+++ api/inference.py
@@ -4,10 +4,20 @@ import cv2
 def softmax(x):
     return np.exp(x)/sum(np.exp(x))
 
-def predict_class(model, contents):
+def read_image(contents):
     nparr = np.fromstring(contents, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     img = np.float32(img)
+    return img
+
+def verify_image(model, img):
+    img = cv2.resize(img, (32, 32)) / 255.0
+    nx, ny, nrgb = img.shape
+    img = img.reshape(1, nx*ny*nrgb)
+    pred_class = model.predict(img)[0]
+    return pred_class
+
+def predict_class(model, img):
     img = cv2.resize(img, (299, 299))
     img = img[np.newaxis, :]
     pred_classes = model.predict(img)[0]
--- api/main.py
+++ api/main.py
@@ -1,35 +1,37 @@
+import joblib
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
-from api.inference import predict_class, softmax
+from api.inference import predict_class, read_image, verify_image, softmax
 import tensorflow as tf
 from tensorflow import keras
 import tensorflow_addons as tfa
 
-MODEL = keras.models.load_model('models/inception_v3-demo1.h5')
-# CLASS_DICT = {
-#     0: '더뎅이병, 검은별무늬병', # scab
-#     1: '건강함', # healthy
-#     2: '점무늬병', # frog_eye_leaf_spot
-#     3: '녹병', # rust
-#     4: '복합성 질병', # complex
-#     5: '흰가루병', # powdery_mildew
-# }
+MODEL = keras.models.load_model('models/inception_v3-230502-91.h5')
+BINARY_MODEL = joblib.load('models/leafs_binary_svm.pkl') 
 CLASS_DICT = {
     0: '복합성 질병', # complex
     1: '점무늬병', # frog_eye_leaf_spot
-    2: '복합성 점무늬병', # frog_eye_leaf_spot complex
-    3: '건강함', # healthy
-    4: '흰가루병', # powdery_mildew
-    5: '복합성 흰가루병', # powdery_mildew complex
-    6: '녹병', # rust
-    7: '복합성 녹병', # rust complex
-    8: '녹병 및 점무늬병', # rust frog_eye_leaf_spot
-    9: '검은별무늬병', # scab
-    10: '검은별무늬병 및 점무늬병', # scab frog_eye_leaf_spot
-    11: '복합성 검은별무늬병 및 점무늬병', # scab frog_eye_leaf_spot complex
+    2: '건강함', # healthy
+    3: '흰가루병', # powdery_mildew
+    4: '녹병', # rust
+    5: '더뎅이병, 검은별무늬병', # scab
 }
+# CLASS_DICT = {
+#     0: '복합성 질병', # complex
+#     1: '점무늬병', # frog_eye_leaf_spot
+#     2: '복합성 점무늬병', # frog_eye_leaf_spot complex
+#     3: '건강함', # healthy
+#     4: '흰가루병', # powdery_mildew
+#     5: '복합성 흰가루병', # powdery_mildew complex
+#     6: '녹병', # rust
+#     7: '복합성 녹병', # rust complex
+#     8: '녹병 및 점무늬병', # rust frog_eye_leaf_spot
+#     9: '검은별무늬병', # scab
+#     10: '검은별무늬병 및 점무늬병', # scab frog_eye_leaf_spot
+#     11: '복합성 검은별무늬병 및 점무늬병', # scab frog_eye_leaf_spot complex
+# }
 
 app = FastAPI()
 
@@ -49,11 +51,18 @@ async def root():
 @app.post("/inference/")
 async def inference_image(file: UploadFile):
     contents = await file.read()
-    pred_classes = predict_class(MODEL, contents)
+    img = read_image(contents)
+    image_class = verify_image(BINARY_MODEL, img)
 
-    pred_list = sorted([(CLASS_DICT[i], p) for i, p in enumerate(pred_classes)], key=lambda tup: tup[1], reverse=True)[:6]
-    prob_list = softmax([p for c, p in pred_list])
-    pred_list = [{'name': c, 'percentage': round(p*100, 1)} for c, p in zip([c for c, p in pred_list], prob_list)]
-    
-    res = {'results': pred_list}
-    return JSONResponse(content=res)
+    if image_class == 0:
+        res = {'results': [{'name': '식물 잎 이미지가 아닙니다.', 'percentage': 0}]}
+        return JSONResponse(content=res)
+    else:
+        pred_classes = predict_class(MODEL, img)
+
+        pred_list = sorted([(CLASS_DICT[i], p) for i, p in enumerate(pred_classes)], key=lambda tup: tup[1], reverse=True)[:6]
+        prob_list = softmax([p for c, p in pred_list])
+        pred_list = [{'name': c, 'percentage': round(p*100, 1)} for c, p in zip([c for c, p in pred_list], prob_list)]
+        
+        res = {'results': pred_list}
+        return JSONResponse(content=res)
